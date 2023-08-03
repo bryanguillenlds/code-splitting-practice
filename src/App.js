9cba2a57c62a9c0869caf6453179cf201f8cdc49
bryan <bryanguillenlds@gmail.com>
2023-08-03 16:58:41 -0600

    Got component ready with basic routing to start working on code splitting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState} from "react";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
 
 function App() {
+  const [route, setRoute] = useState('page1');
+
+  const displayPage = (route) => {
+    switch (route) {
+      case 'page1':
+        return <Page1 onRouteChange={setRoute}/>;
+      case 'page2':
+        return <Page2 onRouteChange={setRoute}/>;
+      case 'page3':
+        return <Page3 onRouteChange={setRoute}/>;
+      default:
+        return <Page1 onRouteChange={setRoute}/>;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {displayPage(route)}
     </div>
   );
 }
